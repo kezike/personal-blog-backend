2fda0617bd146cfd6d5b07844a86ce2a84e3b224
Kayode Ezike <ayo@ezike.io>
2022-12-09 14:00:21 -0500

    improved comment in deploy script

--- deploy.sh
+++ deploy.sh
@@ -4,8 +4,9 @@
 # $1 - npm binary
 
 echo building blog backend...
-# Note - This command is getting killed on VPS;
-# until further notice, build on personal computer
+# Note - This command is getting killed on VPS.
+# Until further notice, build on personal computer
+# and transfer zipped build folder to VPS.
 # yarn build
 echo successfully built blog backend!
 echo running blog backend...
